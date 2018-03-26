da86daa778679e15d17a42620b25c3406f998948
Kranti Nebhwani <darkphoton20@gmail.com>
2018-03-26 20:58:20 +0800

    added navigation menu scrolling functionality.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
-/* section highlighting */
 // global
-var bgTextHighlight = "#82ccdd";
+var bgTextHighlight = "#25CCF7";
 var bgTextOriginal = "#ffffff";
 var head = document.getElementById("header");
 
@@ -18,41 +17,75 @@ var nav = document.getElementById("navList");
 var navOffsetThreshold = -23;
 var headThreshold = 20;
 
+
 //console.log(aboutSectionBG);
 
+
+
+function navAdapt() {
+  window.addEventListener('scroll', function(e) {
+    let viewportOffsetHead = head.getBoundingClientRect();
+    let viewportOffsetNav = nav.getBoundingClientRect();
+    // these are relative to the viewport, i.e. the window
+    let topHead = viewportOffsetHead.top; // offset of header element's start position relative with top of viewport
+    let topNav = viewportOffsetNav.top; // offset of nav element's start position relative with top of viewport
+
+    if(topNav<navOffsetThreshold) {
+      nav.classList.add("scrollNav");
+    } else {
+      nav.classList.remove("scrollNav");
+    }
+
+    // change style back to original if scrolled close to the top
+    if(topHead > headThreshold) {
+      aboutSection.style.fontSize = "16px";
+      aboutSection.style.backgroundColor  = bgTextOriginal;
+      aboutSection.style.boxShadow = "0px 0px 0px 0px";
+      expSection.style.fontSize = "16px";
+      expSection.style.backgroundColor = bgTextOriginal;
+      expSection.style.boxShadow = "0px 0px 0px 0px";
+    }
+
+  });
+}
+
 /* Section Highlighting */
+function sectionHighlighting() {
 
-navAbout.addEventListener('click', function() {
 
-  if(aboutSection.style.backgroundColor != bgTextHighlight) {
-    aboutSection.style.backgroundColor = bgTextHighlight;
-    expSection.style.backgroundColor = bgTextOriginal;
-  }
-});
+  navAbout.addEventListener('click', function() {
 
-navExperience.addEventListener('click', function() {
-  if(expSection.style.backgroundColor != bgTextHighlight) {
-    expSection.style.backgroundColor = bgTextHighlight;
-    aboutSection.style.backgroundColor  = bgTextOriginal;
-  }
-});
+    if(aboutSection.style.backgroundColor != bgTextHighlight) {
+      aboutSection.style.backgroundColor = bgTextHighlight;
+      aboutSection.style.fontSize = "18px";
+      aboutSection.style.boxShadow = "0px 50px 30px -30px";
+      expSection.style.backgroundColor = bgTextOriginal;
+      expSection.style.fontSize = "16px";
+      expSection.style.boxShadow = "0px 0px 0px 0px";
+    }
+  });
 
-window.addEventListener('scroll', function(e) {
-  let viewportOffsetHead = head.getBoundingClientRect();
-  let viewportOffsetNav = nav.getBoundingClientRect();
-  // these are relative to the viewport, i.e. the window
-  let topHead = viewportOffsetHead.top; // offset of header element's start position relative with top of viewport
-  let topNav = viewportOffsetNav.top; // offset of nav element's start position relative with top of viewport
+  navExperience.addEventListener('click', function() {
+    if(expSection.style.backgroundColor != bgTextHighlight) {
+      expSection.style.backgroundColor = bgTextHighlight;
+      expSection.style.fontSize = "18px";
+      expSection.style.boxShadow = "0px 50px 30px -30px";
+      aboutSection.style.backgroundColor  = bgTextOriginal;
+      aboutSection.style.fontSize = "16px";
+      aboutSection.style.boxShadow = "0px 0px 0px 0px";
+    }
+  });
 
-  if(topNav<navOffsetThreshold) {
-    nav.classList.add("scrollNav");
-  } else {
-    nav.classList.remove("scrollNav");
-  }
+  navAdapt();
+}
 
-  if(topHead > headThreshold) {
-    expSection.style.backgroundColor = bgTextOriginal;
-    aboutSection.style.backgroundColor  = bgTextOriginal;
-  }
+// initiate order
+function init() {
+  // activate nav highlighting
+  sectionHighlighting();
+}
 
-});
+
+
+//initiate web app
+init();
